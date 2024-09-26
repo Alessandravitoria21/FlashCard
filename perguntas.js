78c1b891c1249d815b1e5380eb5b5214414dd80a
Alessandravitoria21 <159548876+Alessandravitoria21@users.noreply.github.com>
2024-09-26 12:12:31 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -11,7 +11,82 @@ criaCartao(
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Arte',
+    'Qual seu tipo de arte favorito?',
+    'O meu tipo é pintura'
+)
+criaCartao(
+    'moda',
+    'Qual é o seu tipo de roupa?',
+    'O meu tipo é mais moderno pro básico'
+)
+CriaCartao(
+    'Culinária',
+    'Qual sua comida favorita?',
+    'Aminha favorita é pizza'
+)
+riaCartao(
+    'Imaginação',
+    'Qual seria seu animal de estimação mágico?',
+    'Uma fada, teria de estimação'
+)
+riaCartao(
+    'Música',
+    'Qual música favorita para dançar na chuva?',
+    'Dancing in the Dark do Bruce Springsteen'
+)
+criaCartao(
+    'Força',
+    'Qual é a sua maior força',
+    'A minha maior força é a persistência '
+)
+criaCartao(
+    'Medo',
+    'Qual seu maior medo?',
+    'Meu maior medo é perder as pessoas que eu mais amo'
+)
+criaCartao(
+    'Fraqueza',
+    'Qual sua maiorfraqueza?',
+    'Minha maior fraqueza é não ser boa o suficiente'
+)
+criaCartao(
+    'Amor',
+    'Qua é a sua maior paixão?',
+    'Minha maior paixão é estar com pessoas que me amam'
+)
+criaCartao(
+    'Arrependimento',
+    'Qual foi seu arrependimento?',
+    'O meu arrependimento foi não ter aproveitado minha infância'
+)
+criaCartao(
+    'Sonho',
+    'qual seu mair sonho?',
+    'Meu maior sonho é ter uma família, casa própria e crescer na vida'
+)
+criaCartao(
+    'Conquista',
+    'Qual foi sua maior conquista?',
+    'Foi poder ter minha casa alugada e crescendo na vida, dando orgulho a minha mãe'
+)
+criaCartao(
+    'Desafio',
+    'qual seu maior desafio?',
+    'O meu desafio é poder ter minha casa própria e será um grande desafio'
+)
+criaCartao(
+    'Aprender',
+    'Qual seu maior aprendizado?',
+    'Omeu aprendizado é que a vida é uma jornada de um aprendizado'
+)
+criaCartao(
+    'presente',
+    'qual seu maior presente?',
+    'O meu ,aior presente é ter saúde, uma famĺia incrível e sendo alguém na vida'
+)
+criaCartao(
+    'Objetivo',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
 )
