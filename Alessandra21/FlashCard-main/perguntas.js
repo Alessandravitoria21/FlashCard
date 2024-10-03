6f79001fea362b7008e7a84cfe97409e9e4aed3b
Alessandravitoria21 <159548876+Alessandravitoria21@users.noreply.github.com>
2024-10-03 12:12:03 -0300

    Add files via upload

---
+++ Alessandra21/FlashCard-main/perguntas.js
@@ -0,0 +1,112 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Arte',
+    'Qual seu tipo de arte favorito?',
+    'O meu tipo é pintura'
+)
+criaCartao(
+    'Moda',
+    'qual seu tipo de roupa?',
+    'O meu tipo é mais moderno pro básico'
+)
+CriaCartao(
+    'Culinária',
+    'Qual sua comida favorita?',
+    'Aminha favorita é pizza'
+)
+CriaCartao(
+    'Imaginação',
+    'Qual seria seu animal de estimação mágico?',
+    'Uma fada, teria de estimação'
+)
+criaCartao(
+    'Música',
+    'Qual música favorita para dançar na chuva?',
+    'Dancing in the Dark do Bruce Springsteen'
+)
+criaCartao(
+    'Força',
+    'Qual é a sua maior força',
+    'A minha maior força é a persistência '
+)
+criaCartao(
+    'Medo',
+    'Qual seu maior medo?',
+    'Meu maior medo é perder as pessoas que eu mais amo'
+)
+criaCartao(
+    'Fraqueza',
+    'Qual sua maiorfraqueza?',
+    'Minha maior fraqueza é não ser boa o suficiente'
+)
+criaCartao(
+    'Amor',
+    'Qua é a sua maior paixão?',
+    'Minha maior paixão é estar com pessoas que me amam'
+)
+criaCartao(
+    'Arrependimento',
+    'Qual foi seu arrependimento?',
+    'O meu arrependimento foi não ter aproveitado minha infância'
+)
+criaCartao(
+    'Sonho',
+    'qual seu mair sonho?',
+    'Meu maior sonho é ter uma família, casa própria e crescer na vida'
+)
+criaCartao(
+    'Conquista',
+    'Qual foi sua maior conquista?',
+    'Foi poder ter minha casa alugada e crescendo na vida, dando orgulho a minha mãe'
+)
+criaCartao(
+    'Desafio',
+    'qual seu maior desafio?',
+    'O meu desafio é poder ter minha casa própria e será um grande desafio'
+)
+criaCartao(
+    'Aprender',
+    'Qual seu maior aprendizado?',
+    'Omeu aprendizado é que a vida é uma jornada de um aprendizado'
+)
+criaCartao(
+    'presente',
+    'qual seu maior presente?',
+    'O meu,maior presente é ter saúde, uma famĺia incrível e sendo alguém na vida'
+)
+criaCartao(
+    'Objetivo',
+    'Qual sua flor favorita?',
+    'A minha flor favorita é a rosa. Adoro sua beleza, seu aroma e sua simbologia de amor e paixão'
+)
+criaCartao(
+    'Cor',
+    'Qual sua cor favorita?',
+    'Minha cor favorita é vermelho, adoro uma paixão ardente com brilho'
+)
+criaCartao(
+    'Hobby',
+    'Qual seu hobby favorito?',
+    'O meu hobby favorito é bricar com meu pet, maior amor da minha vida'
+)
+criaCartao(
+    'Perfume',
+    'Qual seu perfume favorito?',
+    'O meu perfume é o Egeo Choc Berry, o seu aroma de chocolate com morango'
+)
+criaCartao(
+    'Filme',
+    'Qual seu filme favorito',
+    'O meu filme favorito é romance, ficção e aventura'
+)
